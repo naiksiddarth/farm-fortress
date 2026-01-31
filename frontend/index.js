48bac226db82dae74662b1e6df432570f62aa42b
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-01-31 17:03:32 +0530

    Add login, signup, healthcheck route

--- frontend/index.js
+++ frontend/index.js
@@ -9,12 +9,25 @@ const static_folder = path.join(__dirname, 'static')
 const app = express()
 app.use(express.static(static_folder))
 
+app.get("/api/healthcheck", (req, res) => {
+    res.status(200)
+    res.send()
+})
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(static_folder, "html/landingpage.html"))
 })
 
 app.get("/dashboard", (req, res) => {
     res.sendFile(path.join(static_folder, "html/dashboard.html"))
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(static_folder, "html/login.html"))
+})
+
+app.get("/signup", (req, res) => {
+    res.sendFile(path.join(static_folder, "html/signup.html"))
 })
 
 app.listen(3000, () => {
