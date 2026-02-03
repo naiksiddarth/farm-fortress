73155b08710f27089513fc6063e762d3654e2a4a
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-02-03 23:49:45 +0530

    Add code to get farm details of registered user

--- backend/controllers/farm.controllers.js
+++ backend/controllers/farm.controllers.js
@@ -1,9 +1,10 @@
 import { Farm } from "../models/farm.models.js"
+import { APIResponse } from "../utils/api-response.js"
 import { asyncHandler } from "../utils/async-handler.js"
 import mongoose from "mongoose"
 
 
-export const createFarm = asyncHandler( async (req, res) => {           //protected
+export const createFarm = asyncHandler(async (req, res) => {           //protected
     const { location, farmSize, farmSizeUnit, soilType, waterSource } = req.body
 
     const farm = await Farm.create({
@@ -15,4 +16,18 @@ export const createFarm = asyncHandler( async (req, res) => {           //protec
         soilType: soilType
     })
     res.status(201).json(farm)
+})
+
+export const getFarm = asyncHandler(async (req, res) => {
+    const user = req.user
+    const farm = await Farm.findOne({ ownedBy: user._id })
+    res
+        .status(200)
+        .json(
+            new APIResponse(200,
+                farm,
+                "farm fetch succesfull"
+            )
+        )
+
 })
