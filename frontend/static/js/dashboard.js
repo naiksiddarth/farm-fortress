1a05759c75f6824878da81635456150355e6e2ff
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-02-04 23:56:13 +0530

    Add code to edit error on unauthrized requests

--- frontend/static/js/dashboard.js
+++ frontend/static/js/dashboard.js
@@ -3,19 +3,35 @@ const errorModal = document.getElementById("error-modal")
 
 const initDashboard = async function () {
     const userData = await fetchData()
-    
+
 }
 
 async function fetchData() {
-    const response = await fetch(`${BACKEND_URL}/farm`)
-    console.log(response.ok)
-    
-    if(!response.ok){
-        return
+
+    try {
+        console.log(errorModal.children[0].children);
+        const response = await fetch(`${BACKEND_URL}/farm`, {
+            credentials: "include"
+        })
+        
+        if (response.status === 401) {
+            errorModal.children[0].children[0].innerHTML = "Unauthorized"
+            errorModal.children[0].children[1].innerHTML = "Please login or signup before accessing this page"
+            errorModal.children[0].children[2].textContent = "Login"
+            errorModal.children[0].children[2].onclick = () => window.location.assign("/login")
+
+            return null
+        }
+        else {
+
+        }
+        errorModal.style.display = "none"
+        const data = await response.json()
+        return data.data
+    } catch (error) {
+        console.error("Fetch error:", error)
+        return null
     }
-    errorModal.style.display = "none"
-    const data = await response.json()
-    return data.data
 }
 
 document.addEventListener("DOMContentLoaded", function () {
