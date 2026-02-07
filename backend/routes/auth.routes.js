dbe81173f7f6f5c02d71d484e5650eceba826543
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-02-07 18:01:18 +0530

    Add GET /api/auth/user route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { registerUser, loginUser, logoutUser, refreshAccessToken} from "../controllers/auth.controllers.js"
+import { registerUser, loginUser, logoutUser, refreshAccessToken, getUser} from "../controllers/auth.controllers.js"
 import { validate } from "../middlewares/validator.middleware.js"
 import { registrationValidator, loginValidator } from "../validators/register.validators.js"
 import { userInjector } from "../middlewares/auth.middleware.js"
@@ -10,5 +10,6 @@ authRouter.route("/register").post(registrationValidator(), validate, registerUs
 authRouter.route("/login").post(loginValidator(), validate, loginUser)
 authRouter.route("/logout").post(userInjector, logoutUser)
 authRouter.route('/refresh-token').post(refreshAccessToken)
+authRouter.route('/user').get(userInjector, getUser)
 
 export default authRouter
