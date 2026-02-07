088c6e83a30344f3a63310ea4c98ecd32798fa5b
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-02-07 17:59:43 +0530

    Add code to not select refresh token and password

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -7,7 +7,7 @@ export const userInjector = async function (req, res, next) {
     if(!accessToken) throw new APIError(401, "No access token found in request")
     try{
         const { _id } = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
-        const user = await User.findOne({ _id: _id})
+        const user = await User.findOne({ _id: _id}).select("-password -refreshToken")
         if(!user) {
             throw new APIError(401, "Invalid JWT token")
         }
