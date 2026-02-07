9b61976ab3b10949d2ea60f82483cdc881353fb8
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-02-07 16:26:24 +0530

    Add code to return farm and some enums

--- backend/controllers/farm.controllers.js
+++ backend/controllers/farm.controllers.js
@@ -2,7 +2,7 @@ import { Farm } from "../models/farm.models.js"
 import { APIResponse } from "../utils/api-response.js"
 import { asyncHandler } from "../utils/async-handler.js"
 import mongoose from "mongoose"
-
+import * as enums from "../utils/constants.js"
 
 export const createFarm = asyncHandler(async (req, res) => {           //protected
     const { location, farmSize, farmSizeUnit, soilType, waterSource } = req.body
@@ -25,7 +25,14 @@ export const getFarm = asyncHandler(async (req, res) => {           //protected
         .status(200)
         .json(
             new APIResponse(200,
-                farm,
+                {
+                    farm: farm,
+                    enum: {
+                        ...enums.soilTypeDisplayName,
+                        ...enums.landSizeUnitDisplayName
+                    }
+
+                },
                 "farm fetch succesfull"
             )
         )
