1d01d4fbb411968866e5b52dbb7a24853829d64e
Siddarth Naik <siddarthnaik2131@gmail.com>
2026-02-07 18:01:56 +0530

    Add error handler middleware

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
@@ -21,5 +22,15 @@ app.use(
 app.use("/api/healthcheck", healthCheckRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/farm", farmRouter)
+
+app.use((error, req, res, next) => {
+    res
+        .status(error.statusCode)
+        .json({
+            status: error.statusCode,
+            message: error.message
+        })
+        return
+})
 
 export default app
